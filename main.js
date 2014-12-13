e1ccd7ebbf23211951350f1008ffa7fd3507c151
Meena Boppana <boppana@college.harvard.edu>
2014-12-12 20:10:21 -0500

    Adding js

---
+++ main.js
@@ -0,0 +1,7 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello world!';
+
+ <FORM>
+   <INPUT TYPE = "text" Value = "dont change" NAME = "leave button"
+   onChange = "alert('Please don't change this')">
+ </FORM>   
